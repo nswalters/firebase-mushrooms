ebb6a140a2b6487f91534e20e7ced07bceb1f086
Nick Walters <nswalters.s@gmail.com>
2020-06-23 18:19:31 -0500

    Display single mycologist card

--- src/javascripts/components/singleMycologist/singleMycologist.js
+++ src/javascripts/components/singleMycologist/singleMycologist.js
@@ -1,12 +1,25 @@
 import mycologistData from '../../helpers/data/mycologistData';
+import utils from '../../helpers/utils';
+
+import './singleMycologist.scss';
 
 const buildMycologist = (e) => {
   const mycologistId = e.target.closest('.card').id;
 
   mycologistData.getMycologistById(mycologistId)
     .then((response) => {
-      // todo use response.data to create a single mycologist div/card/whatnot
-      console.error(response.data);
+      const mycologist = response.data;
+      const domString = `
+        <h2 class="text-center">Featured Mycologist</h2>
+        <div class="col-12">
+          <div class="card text-white bg-dark border-0 rounded-0">
+            <div class="card-header">Mycologist ${mycologist.name} (Age: ${mycologist.age})</div>
+            <div class="card-body"></div>
+          </div>
+        </div>
+      `;
+
+      utils.printToDom('#single-myco', domString);
     })
     .catch((err) => console.error('problem with single mycologist', err));
 };
